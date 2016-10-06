4e9597537b3f3cea4ff9958b03c670e2a3350c5a
Pierre Portejoie <portejoiepierre@gmail.com>
2016-10-06 10:07:45 +0200

    put static homepage (70%)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,61 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        //basic info
+        $age = date('Y',time()) - date('Y', mktime(00,00,00,01,15,1991));
+        $address = [
+            'street' => '29, quai Saint Vincent',
+            'postalCode' => '69001',
+            'city' => 'Lyon',
+            'country' => 'France'
+        ];
+
+        $experiences = array();
+        //experience
+        if (($handle = fopen(__DIR__.'/data/experience_fr.csv', "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+                switch ($data[2]){
+                    case '1':
+                        $data[2] = 'main';
+                        break;
+                    case '2':
+                        $data[2] = 'second';
+                        break;
+                    case '3':
+                        $data[2] = 'third';
+                        break;
+                }
+                array_push($experiences,[
+                    'employer' => $data[0],
+                    'job' => $data[1],
+                    'category' => $data[2],
+                    'start' => $data[3],
+                    'end' => $data[4],
+                    'place' => $data[5],
+                    'website' => $data[6],
+                    'link' => $data[7],
+                    'description' => $data[8]
+                ]);
+            }
+            fclose($handle);
+        }
+        array_shift($experiences);
+
+        //skills
+        $json_file = file_get_contents(__DIR__.'/data/skills.json');
+        $json_file = preg_replace('/\n/', '',$json_file);
+        $skills = json_decode($json_file,true);
+
+
+        $profile = [
+            'age' => $age,
+            'address' => $address
+        ];
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'profile' => $profile,
+            'experiences' => $experiences,
+            'skills' => $skills
         ]);
     }
 }
---
+++ web/assets/js/homepage.js
@@ -0,0 +1,171 @@
+$(document).ready(function(){
+    applyHeader();
+    colorTheSkills();
+    var index = 0;
+    $('.slideshow').hide();
+    $('.slideshow:first').toggle();
+    window.setInterval(function(){
+        $('.slideshow:nth-of-type('+(index%5+1)+')').toggle();
+        $('.slideshow:nth-of-type('+((index+1)%5+1)+')').toggle();
+        index = (index+1)%5;
+    }, 3000);
+});
+
+
+
+function applyHeader() {
+    $('header').css({ height: ($(window).height()) +'px' });
+}
+
+function colorTheSkills() {
+    colorSkills('#svg1', 8, '#E84A5F', '#2A363B');
+    colorSkills('#svg2', 8, '#E84A5F', '#2A363B');
+    colorSkills('#svg3', 3, '#E84A5F', '#2A363B');
+}
+
+function colorSkills(elemt, grade, col1, col2) {
+    var gElems = [$(elemt +' path:nth-of-type(3)'), $(elemt +' path:nth-of-type(7)'), $(elemt +' path:nth-of-type(5)'), $(elemt +' path:nth-of-type(8)'), $(elemt +' path:nth-of-type(6)'), $(elemt +' path:nth-of-type(4)'), $(elemt +' path:nth-of-type(2)'), $(elemt +' path:nth-of-type(1)')];
+
+    for (var i = 0; i < 8; i++){
+        if ((i) < grade ) gElems[i].attr("fill", col1);
+        else gElems[i].attr("fill", col2)
+    }
+}
+
+function showOnlyWeb(){
+    if (jQuery('.portfolio-btn:not(.clicked)').get(0) === jQuery('#cat-w-btn').get(0) &&
+        jQuery('.portfolio-btn:not(.clicked)').get(1) === undefined){
+        jQuery('.portfolio-btn').removeClass('clicked');
+        $('.img-cont:not(:first)').fadeIn();
+    } else {
+        jQuery('.portfolio-btn:not(#cat-w-btn)').addClass('clicked');
+        jQuery('#cat-w-btn').removeClass('clicked');
+        $('.img-cont:not(:first):not(.cat-w)').fadeOut();
+        $('.img-cont:not(:first).cat-w').fadeIn();
+    }
+}
+function showOnlyRes(){
+    if (jQuery('.portfolio-btn:not(.clicked)').get(0) === jQuery('#cat-i-btn').get(0) &&
+        jQuery('.portfolio-btn:not(.clicked)').get(1) === undefined){
+        jQuery('.portfolio-btn').removeClass('clicked');
+        $('.img-cont:not(:first)').fadeIn();
+    } else {
+        jQuery('.portfolio-btn:not(#cat-i-btn)').addClass('clicked');
+        jQuery('#cat-i-btn').removeClass('clicked');
+        $('.img-cont:not(:first):not(.cat-i)').fadeOut();
+        $('.img-cont:not(:first).cat-i').fadeIn();
+    }
+}
+function showOnlyCre(){
+    if (jQuery('.portfolio-btn:not(.clicked)').get(0) === jQuery('#cat-s-btn').get(0) &&
+        jQuery('.portfolio-btn:not(.clicked)').get(1) === jQuery('#cat-c-btn').get(0) &&
+        jQuery('.portfolio-btn:not(.clicked)').get(2) === undefined){
+        jQuery('.portfolio-btn').removeClass('clicked');
+        $('.img-cont:not(:first)').fadeIn();
+    } else {
+        jQuery('.portfolio-btn:not(#cat-s-btn):not(#cat-c-btn)').addClass('clicked');
+        jQuery('#cat-s-btn').removeClass('clicked');
+        jQuery('#cat-c-btn').removeClass('clicked');
+        $('.img-cont:not(:first):not(.cat-s):not(.cat-c)').fadeOut();
+        $('.img-cont:not(:first).cat-s').fadeIn();
+        $('.img-cont:not(:first).cat-c').fadeIn();
+    }
+}
+
+function showHideProjects (button, type) {
+    $(button).toggleClass('clicked');
+    $('.'+type).fadeToggle();
+}
+
+function showHideExperiences (button, type) {
+    $(button).toggleClass('clicked');
+    if ($('.experience.'+type+' .line').css("display") == 'none'){
+        $('.experience.'+type+' .line').toggle(0);
+        $('.experience.'+type+' .metro').toggle(0);
+        $('.experience.'+type).slideToggle();
+    } else {
+        $('.experience.'+type).slideToggle();
+        $('.experience.'+type+' .line').delay(400).toggle(0);
+        $('.experience.'+type+' .metro').delay(400).toggle(0);
+    }
+}
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+
+jQuery('h2').attr("data-sr","move 0px, scale down 20%");
+jQuery('#skills .row .row').attr("data-sr","");
+
+jQuery('#skills .row h3').attr("data-sr","");
+
+
+jQuery('.navbar').hide();
+
+jQuery('form .btn').attr("data-sr","");
+
+jQuery('i').attr("data-sr","move 0px, scale up 50%");
+
+jQuery('.skill-wheel .col-md-3').attr("data-sr","move 0px, scale 0%");
+
+
+function triggerSkillsAnim(e){
+    var time = 20;
+    if (jQuery(e).hasClass('skill-wheel')){
+        jQuery(e).find('svg').find('path:nth-of-type(3)').delay(5*time).fadeToggle();
+        jQuery(e).find('svg').find('path:nth-of-type(7)').delay(10*time).fadeToggle();
+        jQuery(e).find('svg').find('path:nth-of-type(5)').delay(15*time).fadeToggle();
+        jQuery(e).find('svg').find('path:nth-of-type(8)').delay(20*time).fadeToggle();
+        jQuery(e).find('svg').find('path:nth-of-type(6)').delay(25*time).fadeToggle();
+        jQuery(e).find('svg').find('path:nth-of-type(4)').delay(30*time).fadeToggle();
+        jQuery(e).find('svg').find('path:nth-of-type(2)').delay(35*time).fadeToggle();
+        jQuery(e).find('svg').find('path:nth-of-type(1)').delay(40*time).fadeToggle();
+    } else if (jQuery(e).hasClass('skillAnim')) {
+        jQuery(e).find('li').find('.glyphicon:nth-of-type(5)').delay(8*time).fadeToggle();
+        jQuery(e).find('li').find('.glyphicon:nth-of-type(4)').delay(16*time).fadeToggle();
+        jQuery(e).find('li').find('.glyphicon:nth-of-type(3)').delay(24*time).fadeToggle();
+        jQuery(e).find('li').find('.glyphicon:nth-of-type(2)').delay(32*time).fadeToggle();
+        jQuery(e).find('li').find('.glyphicon:nth-of-type(1)').delay(40*time).fadeToggle();
+    } else if (jQuery(e).hasClass('img-circle')) {
+        jQuery('.navbar').toggle("slide");
+    }
+}
+window.sr = new ScrollReveal({complete : function(e) {triggerSkillsAnim(e) }});
+$(function() {
+    $('body').on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$('body').scrollspy({
+    target: '.navbar-default',
+    offset: 100
+});
+
+$('body').scrollspy({
+    target: '.sticky-navbar'
+});
+
+$('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+    $('.navbar-toggle:visible').click();
+});
