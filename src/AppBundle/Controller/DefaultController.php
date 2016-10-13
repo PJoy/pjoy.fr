5c9d0537255c49dcb1016e4d956326566b548832
Pierre Portejoie <portejoiepierre@gmail.com>
2016-10-13 23:01:29 +0200

    portfolio wip

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -58,6 +58,46 @@ class DefaultController extends Controller
         $json_file = preg_replace('/\n/', '',$json_file);
         $skills = json_decode($json_file,true);
 
+        //portfolio
+        $projects = [];
+        if (($handle = fopen(__DIR__.'/data/portfolio.csv', "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+                $tags = explode(',',$data[3]);
+                $images = explode(',',$data[4]);
+                array_push($projects,[
+                    'title' => $data[0],
+                    'start' => $data[1],
+                    'end' => $data[2],
+                    'tags' => $tags,
+                    'images' => $images,
+                    'shortdesc' => $data[5],
+                    'desc' => $data[6]
+                ]);
+            }
+            fclose($handle);
+        }
+        array_shift($projects);
+        usort($projects, function ($item1, $item2) {
+            if ($item1['start'] == $item2['start']) return 0;
+            return $item1['start'] > $item2['start'] ? -1 : 1;
+        });
+
+        $projectCats = [
+            'catName',                      //0
+            'développement web front-end',  //1     a
+            'développement web back-end',   //2     a
+            'graphisme',                    //3     b
+            'développement créatif',        //4     b
+            'traitement d\'images',         //5     c
+            'génération procédurale',       //6     d
+            'jeu vidéo',                    //7     b
+            'développement embarqué',       //8     a
+            'génération récursive',         //9     d
+            'recherche',                    //10    e
+            'formation',                    //11    e
+            'son'                           //12    b
+        ];
+        //array_shift($projectCats);
 
         $profile = [
             'age' => $age,
@@ -67,7 +107,9 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'profile' => $profile,
             'experiences' => $experiences,
-            'skills' => $skills
+            'skills' => $skills,
+            'projects' => $projects,
+            'projectCats' => $projectCats
         ]);
     }
 }
